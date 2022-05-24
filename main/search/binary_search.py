111fe416a670aef050243a33ed7cac965c4718f4
Todd Roling <Todd.Roling@gmail.com>
2022-05-23 20:57:12 -0600

    renamed a param

--- main/search/binary_search.py
+++ main/search/binary_search.py
@@ -1,13 +1,13 @@
-def binary_search(list_, target):
+def binary_search(search_list, target):
 
     first_index = 0
-    last_index = len(list_) - 1
+    last_index = len(search_list) - 1
 
     while first_index <= last_index:
         middle_index = int((first_index + last_index) / 2)
-        if list_[middle_index] == target:
+        if search_list[middle_index] == target:
             return middle_index
-        elif list_[middle_index] < target:
+        elif search_list[middle_index] < target:
             first_index = middle_index + 1
         else:
             last_index = middle_index - 1
