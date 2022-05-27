c36a290ef9d052868fe0d3fa1c292d7a444af4a5
Todd Roling <Todd.Roling@gmail.com>
2022-05-27 12:06:44 -0600

    factorial iterative and recursive

---
+++ main/math/factorial.py
@@ -0,0 +1,17 @@
+def factorial_iterative(n):
+    if n < 0:
+        return -1
+    result = 1
+    if n == 0:
+        return result
+    for i in range(n, 1, -1):
+        result *= i
+    return result
+
+
+def factorial_recursive(n):
+    if n < 0:
+        return -1
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
