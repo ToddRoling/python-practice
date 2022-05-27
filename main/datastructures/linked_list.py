7d8468d779dbebd24df705362c3e3ab02f7471df
Todd Roling <Todd.Roling@gmail.com>
2022-05-26 18:08:37 -0600

    remove unnecessary nested function

--- main/datastructures/linked_list.py
+++ main/datastructures/linked_list.py
@@ -4,18 +4,17 @@ def reverse_singly_linked_list_iterative(head):
     elif head.next is None:
         return head
     else:
-        def _reverse_sll(node):
-            next_node = node.next
-            node.next = None
+        node = head
+        next_node = node.next
+        head.next = None
 
-            while next_node is not None:
-                next_next_node = next_node.next
-                next_node.next = node
-                node = next_node
-                next_node = next_next_node
+        while next_node is not None:
+            next_next_node = next_node.next
+            next_node.next = node
+            node = next_node
+            next_node = next_next_node
 
-            return node
-        return _reverse_sll(head)
+        return node
 
 
 def reverse_singly_linked_list_recursive(head):
@@ -33,5 +32,6 @@ def reverse_singly_linked_list_recursive(head):
                 returned_node = _reverse_sll(node.next)
                 returned_node.next = node
             return node
+
         _reverse_sll(head)
         return head
