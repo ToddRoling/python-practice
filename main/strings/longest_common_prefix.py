f2b7582e1c9cb795d3dd1c14d95c91119e7e1157
Todd Roling <Todd.Roling@gmail.com>
2022-06-01 17:54:26 -0600

    optimized solution for longest_common_prefix.py

--- main/strings/longest_common_prefix.py
+++ main/strings/longest_common_prefix.py
@@ -8,16 +8,21 @@ class Solution:
         if len(arr) == 1:
             return arr[0] if arr[0] else -1
 
-        prefix = min(arr, key=len)
-        arr.remove(prefix)
-
-        common_prefix_found = False
-        while not common_prefix_found and prefix:
-            for string in arr:
-                common_prefix_found = string.startswith(prefix)
-                if not common_prefix_found:
-                    prefix = prefix[:-1]
+        def _get_common_prefix(string_one_, string_two):
+            minimum_length = min(len(string_one_), len(string_two))
+            prefix_ = ''
+            for index_ in range(minimum_length):
+                if string_one_[index_] == string_two[index_]:
+                    prefix_ += string_one_[index_]
+                else:
                     break
+            return prefix_
+
+        prefix = ''
+        for index in range(n - 1):
+            string_one = prefix if prefix else arr[index]
+            prefix = _get_common_prefix(string_one, arr[index + 1])
+            if not prefix:
+                break
 
         return prefix if prefix else -1
-
