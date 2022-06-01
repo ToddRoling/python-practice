135f8aa81370035007e5dc9c9df1b6d5b1fa4f03
Todd Roling <Todd.Roling@gmail.com>
2022-05-31 19:20:47 -0600

    max sub-array sum problem

--- main/datastructures/array.py
+++ main/datastructures/array.py
@@ -20,6 +20,17 @@ class Solution:
                 return False
         return True
 
+    # My solution for https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1/
+    # Function to find contiguous subarray with maximum sum.
+    def maxSubArraySum(self, arr, N):
+        if arr and N == len(arr):
+            maximum_sum = arr[0]
+            current_maximum_sum = arr[0]
+            for index in range(1, N):
+                current_maximum_sum = max(arr[index], current_maximum_sum + arr[index])
+                maximum_sum = max(current_maximum_sum, maximum_sum)
+            return maximum_sum
+
     # My solution to https://practice.geeksforgeeks.org/problems/reverse-array-in-groups0255/1
     # Function to reverse every sub-array group of size k.
     def reverseInGroups(self, arr, N, K):
@@ -37,5 +48,3 @@ class Solution:
                 subgroup_last_index -= 1
 
         return arr
-
-
--- test/test_array.py
+++ test/test_array.py
@@ -1,6 +1,29 @@
+import pytest
+
 from main.datastructures.array import Solution
 
 solution = Solution()
+
+MAX_SUB_ARRAY_SUM_TEST_DATA = [
+    ([], None),
+    ([0, 0], 0),
+    ([0, 0, 1], 1),
+    ([1, 0, -1], 1),
+    ([1, 2, 3, -2, 5], 9),
+    ([-1, -2, -3, -4], -1),
+    ([-9, 900, 901, -1000, 902], 1801),
+    ([0, 10, 0, -10, 0], 10),
+    ([0, -10, 0, 10, 0], 10),
+    ([0, -1, 0, 0, -10000, -20000], 0),
+    ([93, -122, -78, 10, -9, 111, 0], 112)
+]
+
+
+# noinspection PyPep8Naming
+@pytest.mark.parametrize("array, expected_result", MAX_SUB_ARRAY_SUM_TEST_DATA)
+def test_maxSubArraySum(array, expected_result):
+    actual_result = solution.maxSubArraySum(array, len(array))
+    assert actual_result == expected_result
 
 
 def test_array_equality_equal_both_empty():
