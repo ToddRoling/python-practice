39e13573306ac82e6186ceea4503d25308d00975
Todd Roling <Todd.Roling@gmail.com>
2022-06-01 18:05:22 -0600

    first runtime problem

--- main/strings/string_reverse.py
+++ main/strings/string_reverse.py
@@ -5,25 +5,30 @@ def reverse_slice(string):
 def traverse_from_both_ends(string):
     left_index = 0
     right_index = len(string) - 1
+    string_as_list = list(string)
 
     while left_index < right_index:
-        temp = string[left_index]
-        string[left_index] = string[right_index]
-        string[right_index] = temp
+        temp = string_as_list[left_index]
+        string_as_list[left_index] = string_as_list[right_index]
+        string_as_list[right_index] = temp
         left_index += 1
         right_index -= 1
 
-    return string
+    return ''.join(string_as_list)
 
 
 def traverse_to_midpoint(string):
     string_range = range(int(len(string) / 2))
-    string_end_index = len(string) - 1
+    right_index = len(string) - 1
+    string_as_list = list(string)
 
     for left_index in string_range:
-        right_index = string_end_index - left_index
-        temp = string[left_index]
-        string[left_index] = string[right_index]
-        string[right_index] = temp
+        temp = string_as_list[left_index]
+        string_as_list[left_index] = string_as_list[right_index]
+        string_as_list[right_index] = temp
+        right_index -= 1
 
-    return string
+    return ''.join(string_as_list)
+
+
+print(traverse_to_midpoint("hello"))
