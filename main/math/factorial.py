a60de0243a9cc97cbb7bbbc192013ae190cf7855
Todd Roling <Todd.Roling@gmail.com>
2023-08-10 14:58:52 -0600

    Just reviewing code and cleaning up things

--- main/math/factorial.py
+++ main/math/factorial.py
@@ -9,7 +9,7 @@ def factorial_iterative(n):
 
 def factorial_recursive(n):
     if n < 0:
-        return -1
+        return None
     if n < 2:
         return 1
     return n * factorial_recursive(n - 1)
