5d4ebeefa2701bea92bc35b44774d6d89d935507
Todd D. Roling <Todd.Roling@gmail.com>
2025-02-17 18:11:32 -0600

    Add ListNode implementation and addTwoNumbers method for linked list sum

---
+++ src/pythonpractice/datastructures/list/linkedlist/list_node.py
@@ -0,0 +1,5 @@
+# Necessary class for leetcode
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
--- src/pythonpractice/math/collection_sums.py
+++ src/pythonpractice/math/collection_sums.py
@@ -1,3 +1,11 @@
+from pythonpractice.datastructures.list.linkedlist import ListNode
+
+# Define ListNode locally to resolve the error
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def two_sum(list_, target_sum):
     discovered = set()
     result = set()
@@ -9,8 +17,33 @@ def two_sum(list_, target_sum):
         discovered.add(element)
     return result
 
-
 class Solution:
+
+    # seems like O(3n), probably could make O(n)
+    def addTwoNumbers(self, lst1, lst2):
+
+        def build_reversed_num(lst_node):
+            multiplier = 1
+            result = 0
+            while lst_node:
+                num = lst_node.val * multiplier
+                result += num
+                multiplier *= 10
+                lst_node = lst_node.next
+            return result
+
+        def build_reversed_list(num):
+            current = head = ListNode()
+            while num > 0:
+                current.val = num % 10
+                num //= 10
+                if num > 0:
+                    current.next = ListNode()
+                    current = current.next
+            return head
+
+        reversed_sum = build_reversed_num(lst1) + build_reversed_num(lst2)
+        return build_reversed_list(reversed_sum)
 
     # My solution to https://practice.geeksforgeeks.org/problems/find-all-pairs-whose-sum-is-x5808/1
     # noinspection PyPep8Naming,PyMethodMayBeStatic
@@ -23,7 +56,7 @@ class Solution:
             if v in set_b:
                 result.append((u, v))
         return result
-    
+
     # My solution to https://leetcode.com/problems/two-sum
     def twoSum(self, nums, target):
         discovered = dict()
